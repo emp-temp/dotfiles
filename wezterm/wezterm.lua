af5ab15644e6e3e99f5c690e0befdcee25776150
emp-temp <emptemp0000@gmail.com>
2024-04-06 18:10:20 +0900

    first commit

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,69 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.font_size = 14
+
+config.keys = {
+  {
+    key = "\\",
+    mods = "ALT",
+    action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" },
+  },
+  {
+    key = "-",
+    mods = "ALT",
+    action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" },
+  },
+  {
+    key = "k",
+    mods = "ALT",
+    action = wezterm.action.ActivatePaneDirection "Up",
+  },
+  {
+    key = "j",
+    mods = "ALT",
+    action = wezterm.action.ActivatePaneDirection "Down",
+  },
+  {
+    key = "h",
+    mods = "ALT",
+    action = wezterm.action.ActivatePaneDirection "Left",
+  },
+  {
+    key = "l",
+    mods = "ALT",
+    action = wezterm.action.ActivatePaneDirection "Right",
+  },
+  {
+    key = "f",
+    mods = "ALT",
+    action = wezterm.action.ToggleFullScreen,
+  },
+  {
+    key = 't',
+    mods = 'ALT',
+    action = wezterm.action.SpawnTab 'CurrentPaneDomain',
+  },
+  {
+    key = 'w',
+    mods = 'ALT',
+    action = wezterm.action.CloseCurrentTab { confirm = true },
+  },
+}
+
+for i = 1, 8 do
+  -- CTRL+ALT + number to move to that position
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = 'ALT',
+    action = wezterm.action.MoveTab(i - 1),
+  })
+end
+
+return config
+
