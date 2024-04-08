a7277eb773155b118586c44ce0cf4040ab3c29a8
emp-temp <emptemp0000@gmail.com>
2024-04-08 13:48:40 +0900

    refactor: 💡 lunarvimの設定を変更

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,12 +1,16 @@
-lvim.colorscheme = "gruvbox"
+lvim.colorscheme = "catppuccin-frappe"
 
 vim.opt.clipboard = "unnamedplus"
 
 lvim.plugins = {
   { "ellisonleao/gruvbox.nvim" },
+  { "Rigellute/shades-of-purple.vim" },
+  { "catppuccin/nvim" },
   { "github/copilot.vim" },
   { "kdheepak/lazygit.nvim" },
-  { "tpope/vim-fugitive" }
+  { "tpope/vim-fugitive" },
+  { "gotchane/vim-git-commit-prefix" },
+  { "folke/trouble.nvim" }
 }
 
 local formatters = require "lvim.lsp.null-ls.formatters"
